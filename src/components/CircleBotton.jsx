bd8380ddf41a21585f4a0234e4ec7c308ab3702d
渡邉雅広 <watanabemasahiro@masahiro.local>
2023-02-05 12:03:12 +0900

    add memoDetailScreen

--- src/components/CircleBotton.jsx
+++ src/components/CircleBotton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { string } from 'prop-types';
+import { shape, string } from 'prop-types';
 
 export default function CircleBotton(props) {
-  const { children } = props;
+  const { children, style } = props;
   return (
-    <View style={styles.circleBotton}>
+    <View style={[styles.circleBotton, style]}>
       <Text style={styles.circleBottonLabel}>{children}</Text>
     </View>
   );
@@ -13,6 +13,11 @@ export default function CircleBotton(props) {
 
 CircleBotton.propTypes = {
   children: string.isRequired,
+  style: shape(),
+};
+
+CircleBotton.defaultProps = {
+  style: null,
 };
 
 const styles = StyleSheet.create({
---
+++ src/screens/MemoListScreen.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import AppBar from '../components/AppBar';
+import MemoList from '../components/MemoList';
+import CircleBotton from '../components/CircleBotton';
+
+export default function MemoListScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <MemoList />
+      <CircleBotton>+</CircleBotton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+});
